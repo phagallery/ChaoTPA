ea600ebc24d6a26f5fcb1afc3eafb2659fef306b
chayapol <iamchayapol@gmail.com>
2019-07-09 14:06:02 +0700

    Scaffold OK

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:chao_tpa/screens/authen.dart';
+import 'package:flutter/material.dart';
+
+main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Authen(),
+    );
+  }
+}
