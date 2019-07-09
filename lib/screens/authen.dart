e2a3cffd47273fd643bd430df33f447d59dec7c2
chayapol <iamchayapol@gmail.com>
2019-07-09 15:58:32 +0700

    Font OK

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,10 +6,42 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  // Explicit
+
+  // Method
+  Widget showLogo() {
+    return Container(
+      width: 150.0,
+      // height: 120.0,
+      child: Image.asset('images/logo.png'),
+    );
+  }
+
+  Widget showText() {
+    return Text(
+      'Chao TPA',
+      style: TextStyle(
+        fontSize: 30.0,
+        color: Colors.brown[800],
+        fontWeight: FontWeight.bold,
+        fontFamily: 'Pacifico'
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Authen'),
+      body: Container(
+        alignment: Alignment.topCenter,
+        padding: EdgeInsets.only(top: 60.0),
+        child: Column(
+          children: <Widget>[
+            showLogo(),
+            showText(),
+          ],
+        ),
+      ),
     );
   }
 }
